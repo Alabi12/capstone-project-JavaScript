2820df5cfded41d6c69fbc5849976544c32bc75c
Alabi12 <wolo22001@gmail.com>
2022-11-29 20:30:41 +0000

    update the index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import "./css/style.css";
-import loadApi from "./modules/loadApi.js";
-import postComment from "./modules/postComment";
-import getComments from "./modules/comments";
+import './css/style.css';
+import loadApi from './modules/loadApi.js';
+import postComment from './modules/postComment.js';
+import getComments from './modules/comments.js';
 
 // Fetch Data From API
 
-const popup = document.querySelector(".popup");
+const popup = document.querySelector('.popup');
 
 const getMovieDetails = async (movieId) => {
   const response = await fetch(`https://api.tvmaze.com/shows/${movieId}`);
@@ -13,12 +13,11 @@ const getMovieDetails = async (movieId) => {
   return myJson;
 };
 
-const movieTitle = document.querySelector("#main-card");
+const movieTitle = document.querySelector('#main-card');
 const apiData = loadApi().then((data) => {
   data.forEach((movie) => {
     // Get Movie Name
-    let imageUrl =
-      "https://static.tvmaze.com/uploads/images/medium_portrait/4/11308.jpg";
+    let imageUrl = 'https://static.tvmaze.com/uploads/images/medium_portrait/4/11308.jpg';
     // Get Movie Image
     if (movie.show.image) {
       imageUrl = movie.show.image.medium;
@@ -35,20 +34,20 @@ const apiData = loadApi().then((data) => {
       </div>
     </div>`;
 
-    const movieButtons = document.querySelectorAll(".movie-button");
+    const movieButtons = document.querySelectorAll('.movie-button');
     movieButtons.forEach((button) => {
-      button.addEventListener("click", (e) => {
+      button.addEventListener('click', (e) => {
         getMovieDetails(e.target.id).then((data) => {
-          const popupContainer = document.querySelector(".popup-container");
-          let movieName = data.name;
-          let movieDescription = data.summary;
-          let imageSrc = "";
+          const popupContainer = document.querySelector('.popup-container');
+          const movieName = data.name;
+          const movieDescription = data.summary;
+          let imageSrc = '';
           if (data.image) {
             imageSrc = data.image.medium;
           }
 
-          let movieId = data.id;
-          popup.classList.remove("hide");
+          const movieId = data.id;
+          popup.classList.remove('hide');
 
           const firstPart = `
   <div class="popup-movie-banner">
@@ -73,16 +72,16 @@ const apiData = loadApi().then((data) => {
           <div class="popup-comments-container">
             <ul class="comment-description">
             `;
-          let commentList = "";
-          const comments = getComments(movieId).then((data) => {
-            const commentCount = document.querySelector("#totalComment");
+          let commentList = '';
+          getComments(movieId).then((data) => {
+            const commentCount = document.querySelector('#totalComment');
             commentCount.innerHTML = data.length;
 
             data.forEach((comment) => {
               commentList += `<li>${comment.creation_date} ${comment.username}: ${comment.comment}</li>`;
             });
             const commentListContainer = document.querySelector(
-              ".comment-description"
+              '.comment-description',
             );
             commentListContainer.innerHTML = commentList;
           });
@@ -114,13 +113,13 @@ const apiData = loadApi().then((data) => {
 
           popupContainer.innerHTML = firstPart + commentList + secondPart;
 
-          const closeBtn = document.querySelector("#popup-close");
-          closeBtn.addEventListener("click", () => {
-            popup.classList.add("hide");
+          const closeBtn = document.querySelector('#popup-close');
+          closeBtn.addEventListener('click', () => {
+            popup.classList.add('hide');
           });
 
-          const addCommentForm = document.querySelector("#addCommentForm");
-          addCommentForm.addEventListener("submit", (e) => {
+          const addCommentForm = document.querySelector('#addCommentForm');
+          addCommentForm.addEventListener('submit', (e) => {
             e.preventDefault();
             const name = e.target.name.value;
             const comment = e.target.comment.value;
@@ -129,18 +128,18 @@ const apiData = loadApi().then((data) => {
             postComment(movieId, name, comment);
             const d = new Date();
             const commentDescription = document.querySelector(
-              ".comment-description"
+              '.comment-description',
             );
-            const commentConter = document.querySelector("#totalComment");
+            const commentConter = document.querySelector('#totalComment');
 
-            const ye = new Intl.DateTimeFormat("en", {
-              year: "numeric",
+            const ye = new Intl.DateTimeFormat('en', {
+              year: 'numeric',
             }).format(d);
-            const mo = new Intl.DateTimeFormat("en", {
-              month: "2-digit",
+            const mo = new Intl.DateTimeFormat('en', {
+              month: '2-digit',
             }).format(d);
-            const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(
-              d
+            const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(
+              d,
             );
             commentDescription.innerHTML += `<li>${ye}-${mo}-${da} ${name}: ${comment}</li>`;
             commentConter.innerHTML = Number(commentConter.innerHTML) + 1;
